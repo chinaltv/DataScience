ce1e2830f5499d8c39c493e3389ce116862c9fc2
chinaltv <thunderltv@gmail.com>
2022-10-24 18:06:20 +0800

    2022.10.24 日常更改

--- ASD_utilities.py
+++ ASD_utilities.py
@@ -26,7 +26,7 @@ def read_one(InputPaths):
 
 # 该函数已稳定，使用已有的json导入文件（使用内置的read_one函数）并指定背景图和z/y轴理论长度（从程序那边报告来的，unity的东西），生成整个流程的注视点热力图
 
-def json_2Dhm(json_filepath, background_filepath, z_len = [-3.302,3.395], y_len = [0,3.938], interest_vari = 'fp'): # Your json_filepath must be like: './paired_data/ASD/A1.json', your interest_vari must be like: 'fp'
+def json_2Dhm(json_filepath, background_filepath, z_len = [-3.34,3.49], y_len = [-0.02,3.938], interest_vari = 'fp'): # Your json_filepath must be like: './paired_data/ASD/A1.json', your interest_vari must be like: 'fp'
 
     def read_one(InputPaths):
         content = []
@@ -41,26 +41,54 @@ def json_2Dhm(json_filepath, background_filepath, z_len = [-3.302,3.395], y_len 
 
     raw_json = read_one(json_filepath)[interest_vari]
     background = mpimg.imread(background_filepath)
-
-    z_scale = (z_len[1] - z_len[0]) / background.shape[1] # 6.697 / 824
-    y_scale = (y_len[1] - y_len[0]) / background.shape[0] # 3.938 / 490
-
     raw_json = raw_json.values.tolist()
-    z,y,z_f,y_f = [],[],[],[]
+    z,y = [],[]
 
     for dict in raw_json:
-        z.append(-dict['Z'])
+        z.append(z_len[1] - dict['Z'] + z_len[0])
         y.append(dict['Y'])
+    zy_df = pd.DataFrame({'z': z, 'y': y})
+    fig, ax = plt.subplots()
+    ax.imshow(background, extent = [z_len[0], z_len[1], y_len[0], y_len[1]])
+    plt.scatter(zy_df['z'], zy_df['y'], c='r', marker='.', s = 10, alpha = 0.1)
+    return plt.show()
 
-    for elements in z:
-        z_f.append((elements-z_len[0])/z_scale)
-    for elements in y:
-        y_f.append(elements/y_scale)
 
-    zy_df = pd.DataFrame({'z': z_f, 'y': y_f})
-    plt.imshow(background, extent = [min(z_f), max(z_f), min(y_f), max(y_f)])
-    return plt.scatter(zy_df['z'], zy_df['y'], c='r', marker='.', s = 10, alpha = 0.1)
+# def json_2Dhm(json_filepath, background_filepath, z_len = [-3.302,3.395], y_len = [0,3.938], interest_vari = 'fp'): # Your json_filepath must be like: './paired_data/ASD/A1.json', your interest_vari must be like: 'fp'
 
+#     def read_one(InputPaths):
+#         content = []
+#         raw = open(InputPaths, 'r', encoding='utf-8') # 读取文件
+#         for line in raw.readlines():
+#             if line.startswith(u'\ufeff'):
+#                 line = line.encode('utf8')[3:].decode('utf8')
+#             dict = json.loads(line)
+#             content.append(dict)
+#         df = pd.DataFrame(content)
+#         return df
+
+#     def scale(x, srcRange, dstRange):
+#         return (x - srcRange[0]) * (dstRange[1] - dstRange[0]) / (srcRange[1] - srcRange[0]) + dstRange[0]
+
+#     raw_json = read_one(json_filepath)[interest_vari]
+#     background = mpimg.imread(background_filepath)
+#     raw_json = raw_json.values.tolist()
+#     z,y,z_f,y_f = [],[],[],[]
+
+#     for dict in raw_json:
+#         z.append(dict['Z'])
+#         y.append(dict['Y'])
+#     for elements in z:
+#         elements_trans = z_len[1] - elements + z_len[0]
+#         z_f.append(scale(elements_trans, z_len, [0, background.shape[1]]))
+#     for elements in y:
+#         y_f.append(scale(elements, y_len, [0, background.shape[0]]))
+
+#     zy_df = pd.DataFrame({'z': z_f, 'y': y_f})
+#     fig, ax = plt.subplots()
+#     ax.imshow(background, extent = [0, background.shape[1], 0, background.shape[0]])
+#     plt.scatter(zy_df['z'], zy_df['y'], c='r', marker='.', s = 10, alpha = 0.1)
+#     return plt.show()
 
 
 
@@ -81,8 +109,6 @@ def json_2Dhm_fl(json_filepath, ts_filepath, background_filepath, z_len = [-3.30
     ts_df = read_one(ts_filepath)
     raw_json = read_one(json_filepath)
     background = mpimg.imread(background_filepath)
-    z_scale = (z_len[1] - z_len[0]) / background.shape[1]
-    y_scale = (y_len[1] - y_len[0]) / background.shape[0]
 
     SayHello_ts_start = bisect_left(raw_json['ts'].values, ts_df['SayHelloDialogueList'][0][0]['BeforeDialogueAtFirstTs'])
     SayHello_ts_end = bisect_left(raw_json['ts'].values, ts_df['SayHelloDialogueList'][0][0]['AfterDialogueAtFirstTs'])
@@ -91,15 +117,20 @@ def json_2Dhm_fl(json_filepath, ts_filepath, background_filepath, z_len = [-3.30
     SayHello_df = raw_json.iloc[SayHello_ts_start: SayHello_ts_end + 1][interest_vari].values.tolist()
     FinalDialogue_df = raw_json.iloc[FinalDialogue_ts_start: FinalDialogue_ts_end + 1][interest_vari].values.tolist()
 
-    def zy_df(dataframe, z_len = z_len, y_len = y_len, z_scale = z_scale, y_scale = y_scale):
+    def zy_df(dataframe, z_len = z_len, y_len = y_len):
+        
+        def scale(x, srcRange, dstRange):
+            return (x - srcRange[0]) * (dstRange[1] - dstRange[0]) / (srcRange[1] - srcRange[0]) + dstRange[0]
+
         z,y,z_f,y_f = [],[],[],[]
         for dict in dataframe:
-            z.append(-dict['Z'])
+            z.append(dict['Z'])
             y.append(dict['Y'])
         for elements in z:
-            z_f.append((elements-z_len[0])/z_scale)
+            elements_trans = z_len[1] - elements + z_len[0]
+            z_f.append(scale(elements_trans, z_len, [0, background.shape[1]]))
         for elements in y:
-            y_f.append(elements/y_scale)
+            y_f.append(scale(elements, y_len, [0, background.shape[0]]))
         zy_df = pd.DataFrame({'z': z_f, 'y': y_f})
         return zy_df, z_f, y_f
 
--- sklearn notes.py
+++ sklearn notes.py
@@ -4,6 +4,7 @@ Y_train = [0,1,1] # 假数据
 
 
 # KNN
+from random import Random
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(algorithm = "auto", leaf_size = k, metric = "minkowski", metric_params = None, n_jobs = k, n_neighbors= k, p = k, weight = 'uniform')
 knn.fit(X_train, Y_train)
@@ -11,3 +12,20 @@ y_pred = knn.predict()
 
 
 from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators = 500, 
+                                criterion = 'gini', 
+                                max_depth = 7, 
+                                min_samples_leaf = 1, 
+                                min_weight_fraction_leaf = 0, # unmodified
+                                max_features = 2,
+                                max_leaf_nodes = 1,
+                                min_impurity_decrease = 1,
+                                bootstrap = ,
+                                oob_score = ,
+                                n_jobs = ,
+                                random_state = ,
+                                verbose = 0,
+                                warm_start = False,
+                                class_weight = ,
+                                ccp_alpha = ,
+                                max_samples = None)
